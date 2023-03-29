6d13b9d1d3e2563b6968e4457844b7fe5fdccbb7
abhishek <abhishek.nt94@gmail.com>
2023-03-29 13:07:09 -0700

    added Navbar

--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -3,32 +3,48 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Navbarr = () => {
+  const [Activelink,setActivelink] = useState('home');
+  const [Scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+   const onScroll = ()=> {
+    if (window.scrollY> 50){
+
+      setScrolled(true);
+    }
+    else{
+      setScrolled(false)
+    }
+window.addEventListener("scroll",onScroll);
+return ()=> window.removeEventListener("scroll",onScroll)
+   }
+  }, [])
+  
   return (
     <div>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" className={Scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home"><img src={''} alt="logo"/></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#Skills">Skills</Nav.Link>
+              <Nav.Link href="#Project">Project</Nav.Link>
+              
+              
             </Nav>
+            <span className="navbar-text">
+              <div className="social-icons">
+                <a href=""><img src="" alt="" srcset="" /></a>
+                <a href=""><img src="" alt="" srcset="" /></a>
+                <a href=""><img src="" alt="" srcset="" /></a>
+              </div>
+            </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
